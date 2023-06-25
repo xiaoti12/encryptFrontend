42d16df8ec94f38d1937d87664d54ba33059d2c8
ljy <jy-liu@qq.com>
2023-06-25 15:54:39 +0800

    change

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: '加密流量异常检测系统',
+  title: '加密流量恶意检测系统',
   pwa: true,
   // logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   logo: '/buptlogo.svg',
