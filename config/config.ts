f6b343e0a3bc7e2eb090f8bf63654d6278828c87
ljy <jy-liu@qq.com>
2023-07-03 15:19:22 +0800

    add panel

--- config/config.ts
+++ config/config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: '5G核心网检测系统',
+  title: '加密流量检测系统',
   layout: {
     locale: true,
     ...defaultSettings,
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -23,7 +23,7 @@ const Admin: React.FC = () => {
             textAlign: 'center',
           }}
         >
-          <SmileTwoTone /> 5G核心网检测系统 <HeartTwoTone twoToneColor="#eb2f96" /> You
+          <SmileTwoTone /> 加密流量异常检测系统 <HeartTwoTone twoToneColor="#eb2f96" /> You
         </Typography.Title>
       </Card>
       <p
